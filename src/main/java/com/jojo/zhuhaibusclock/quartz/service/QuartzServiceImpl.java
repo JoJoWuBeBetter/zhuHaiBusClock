91cd1fe1c3343a437c166ecf754aec7dcb180b86
JoJoWu <453840856@qq.com>
2021-08-19 16:55:34 +0800

    完成不重复的闹钟使用SimpleTrigger

--- src/main/java/com/jojo/zhuhaibusclock/quartz/service/QuartzServiceImpl.java
+++ src/main/java/com/jojo/zhuhaibusclock/quartz/service/QuartzServiceImpl.java
@@ -6,7 +6,12 @@ import com.jojo.zhuhaibusclock.model.SysClock;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.springframework.stereotype.Service;
-// TODO 0/30 0-2 10 ? * 1,2,3,4,5
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+// TODO  重复闹钟使用CronTrigger
 
 /**
  * @author JoJoWu
@@ -25,33 +30,44 @@ public class QuartzServiceImpl implements QuartzService {
     @Override
     public void addClockSchedule(SysClock clock) {
         log.info("添加闹钟计划");
-        CronTrigger clockTrigger = TriggerBuilder.newTrigger().forJob(jobDetail)
-                .withIdentity(clock.getId().toString(), "clockTrigger")
-                .usingJobData("clockId", clock.getId())
-                .withSchedule(CronScheduleBuilder.cronSchedule("0/1 * * * * ?").withMisfireHandlingInstructionIgnoreMisfires()
-                )
-                .build();
+        Trigger trigger;
+        if (clock.getRepeatTime() == null) {
+            LocalTime now = LocalTime.now();
+            LocalDate nowDate = LocalDate.now();
+            log.info(clock.toString());
+            if (clock.getAlarmTime().compareTo(now) <= 0) {
+                nowDate = nowDate.plusDays(1);
+            }
+
+            Date startDate = Date.from(clock.getAlarmTime().atDate(nowDate).atZone(ZoneId.systemDefault()).toInstant());
+            log.info(startDate.toString());
+
+
+            trigger = TriggerBuilder.newTrigger().forJob(jobDetail)
+                    .withIdentity(clock.getId().toString(), "clockTrigger")
+                    .usingJobData("clockId", clock.getId())
+                    .startAt(startDate)
+                    .withSchedule(
+                            SimpleScheduleBuilder.simpleSchedule().withIntervalInMinutes(1).withRepeatCount(10))
+                    .build();
+        } else {
+            trigger = TriggerBuilder.newTrigger().forJob(jobDetail)
+                    .withIdentity(clock.getId().toString(), "clockTrigger")
+                    .usingJobData("clockId", clock.getId())
+                    .withSchedule(CronScheduleBuilder.cronSchedule("0/1 * * * * ?").withMisfireHandlingInstructionIgnoreMisfires()
+                    )
+                    .build();
+        }
+
 
         try {
-            scheduler.scheduleJob(clockTrigger);
+            scheduler.scheduleJob(trigger);
         } catch (SchedulerException e) {
             log.error("闹钟任务创建失败:" + e.getMessage());
             log.error("任务数据:" + JSON.toJSONString(clock));
             throw new ClockException("闹钟任务创建失败:" + e.getMessage());
         }
     }
-
-//    @Override
-//    public void pauseClockSchedule(Long clockId) {
-//        log.info("暂停闹钟计划");
-//        TriggerKey triggerKey = TriggerKey.triggerKey(clockId.toString(), "clockTrigger");
-//        try {
-//            scheduler.pauseTrigger(triggerKey);
-//        } catch (SchedulerException e) {
-//            log.error("闹钟" + JSON.toJSONString(clockId) + "暂停失败");
-//            throw new ClockException("闹钟任务暂停失败");
-//        }
-//    }
 
     @Override
     public void deleteClockSchedule(Long clockId) {
@@ -64,6 +80,18 @@ public class QuartzServiceImpl implements QuartzService {
             log.error("闹钟" + clockId + "删除失败:" + e.getMessage());
             throw new ClockException("闹钟" + clockId + "删除失败:" + e.getMessage());
         }
+    }
+
+    public static String getClockCronString(SysClock clock) {
+
+        String cronString;
+        cronString = String.format("0/30 %d-%d %d ? * %s",
+                clock.getAlarmTime().plusMinutes(-5).getMinute(),
+                clock.getAlarmTime().getMinute(),
+                clock.getAlarmTime().getHour(),
+                clock.getRepeatTime());
+
+        return cronString;
     }
 
 }
